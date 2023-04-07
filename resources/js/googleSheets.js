b3778de4428c3b1e1e43718b80c45b6693fcb252
treymcmeans <tluck234@gmail.com>
2023-04-07 07:48:04 -0500

    Update googleSheets.js

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -1,5 +1,5 @@
 function setUpGoogleSheets() {
-    const scriptURL = '<SCRIPT URL>'
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyDLT7UmtzGZDspowXNRrbQze2xYJVuHZaHt-5eHS4RgUWspsZ8BWCgKM4NpZfVhAxS/exec';
     const form = document.querySelector('#scoutingForm')
     const btn = document.querySelector('#submit')
  
